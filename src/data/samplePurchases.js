32144f0b44650d1b8cc04aa6fecf6d64a3bd0ab0
Lurd Synell B. Najarila <lnajarila@gbox.adnu.edu.ph>
2025-04-11 07:55:13 +0800

    feature(data): Add sample purchases

---
+++ src/data/samplePurchases.js
@@ -0,0 +1,63 @@
+/**
+ * Author: Lurd Synell B. Najarila
+ * Description: Sample purchases data to initially populate the website.
+ */
+
+export const samplePurchases = [
+  {
+    id: 1,
+    listingId: 3,
+    sellerId: 1,
+    buyerId: 5,
+    purchaseDate: '2025-01-22T09:20:00Z',
+    price: 600.00
+  },
+  {
+    id: 2,
+    listingId: 4,
+    sellerId: 3,
+    buyerId: 7,
+    purchaseDate: '2025-01-25T16:30:00Z',
+    price: 150.00
+  },
+  {
+    id: 3,
+    listingId: 9,
+    sellerId: 9,
+    buyerId: 2,
+    purchaseDate: '2025-02-11T15:00:00Z',
+    price: 400.00
+  },
+  {
+    id: 4,
+    listingId: 16,
+    sellerId: 6,
+    buyerId: 10,
+    purchaseDate: '2025-03-11T14:30:00Z',
+    price: 500.00
+  },
+  {
+    id: 5,
+    listingId: 18,
+    sellerId: 8,
+    buyerId: 4,
+    purchaseDate: '2025-03-17T17:45:00Z',
+    price: 220.00
+  },
+  {
+    id: 6,
+    listingId: 24,
+    sellerId: 5,
+    buyerId: 1,
+    purchaseDate: '2025-04-10T10:00:00Z',
+    price: 150.00
+  },
+  {
+    id: 7,
+    listingId: 29,
+    sellerId: 3,
+    buyerId: 8,
+    purchaseDate: '2025-05-06T11:00:00Z',
+    price: 800.00
+  }
+];
