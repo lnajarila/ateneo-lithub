cbe3150d9d8f14b08f28ab013b4fcb0d9e53f698
Lurd Synell B. Najarila <lnajarila@gbox.adnu.edu.ph>
2025-04-12 16:50:22 +0800

    feature(data): Create context provider for offers data

---
+++ src/context/OfferContext.jsx
@@ -0,0 +1,72 @@
+/**
+ * Author: Lurd Synell B. Najarila
+ * Description: Context provider to manage offer and provide CRUD functions.
+ */
+
+import { createContext, useState } from 'react';
+import { sampleOffers } from '../data/sampleOffers.js';
+
+export const OfferContext = createContext();
+
+export const OfferProvider = ({ children }) => {
+  // Initialize offers from localStorage or sample offers data
+  const [offers, setOffers] = useState(() => {
+    const storedOffers = localStorage.getItem('offers');
+    return storedOffers ? JSON.parse(storedOffers) : sampleOffers;
+  });
+
+  // Save to localStorage whenever offers data changes
+  const saveOffers = (updatedOffers) => {
+    setOffers(updatedOffers);
+    localStorage.setItem('offers', JSON.stringify(updatedOffers));
+  };
+
+  // CREATE: Add a new offer
+  const createOffer = () => {};
+
+  // READ: Get offers
+  const getOffers = () => offers;
+  const getOfferById = (id) => offeres.find((offer) => offer.offerId === id);
+  const getOffersByUserId = (userId, offerStatus) => {
+    return offers
+      .filter((offer) => offer.userId === userId)
+      .filter((offer) => offer.status === offerStatus)
+      .sort((a, b) => {
+        const dateA = new Date(a.offerDate);
+        const dateB = new Date(b.offerDate);
+        return dateB - dateA;
+      });
+  };
+
+  // UPDATE: Modify an existing offer
+  const updateOffers = (id, updatedData) => {
+    const updatedOffers = offers.map((offer) => {
+      offer.offerId === id ? {...offers, ...updatedData} : offer;
+    });
+
+    saveOffers(updatedOffers);
+  };
+
+  // DELETE: Remove an offer
+  const deleteOffer = (id) => {
+    const updatedOffers = offers.filter((offer) => offer.offerId !== id);
+    saveOffers(updateOffers);
+  };
+
+  return (
+    <OfferContext.Provider
+      value={{
+        offers,
+        saveOffers,
+        createOffer,
+        getOffers,
+        getOfferById,
+        getOffersByUserId,
+        updateOffers,
+        deleteOffer,
+      }}
+    >
+      {children}
+    </OfferContext.Provider>
+  );
+};
