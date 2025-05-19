f45df0bf8b25387b258367f06b096f35ba3339ca
James Andrei Aguilar <jaguilar@gbox.adnu.edu.ph>
2025-05-19 20:18:11 +0800

    HomePage: Added home page and fixed Header links

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,11 @@
 import Footer from "../widgets/Footer";
 import RegistrationForm from "../widgets/RegistrationForm";
+import Header from '../widgets/Header.jsx';
 
 function Register() {
   return (
     <>
+      <Header />
       <RegistrationForm />
       <Footer />
     </>
